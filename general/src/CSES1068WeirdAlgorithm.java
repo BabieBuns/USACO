f602f8a86086090e91c496560358dd1b50f8c5b5
BabieBuns <cjkim14912@gmail.com>
2022-06-07 20:11:01 -0700

    finished CSES1068

---
+++ general/src/CSES1068WeirdAlgorithm.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class CSES1068WeirdAlgorithm {
+	
+	static Scanner in;
+	
+	static long n;
+	
+	static StringBuilder output;
+	
+	public static void main (String [] args) {
+		init();
+		solve();
+		output();
+	}
+	
+	public static void init() {
+		in = new Scanner(System.in);
+		
+		n = in.nextLong();
+		
+		output = new StringBuilder();
+	}
+	
+	public static void solve() {
+		output.append(n);
+		
+		long num = n;
+		
+		while (num != 1) {
+			if (num % 2 == 0) {
+				num /= 2;
+			}
+			else {
+				num *= 3;
+				num += 1;
+			}
+			output.append(" ").append(num);
+		}
+	}
+	
+	public static void output() {
+		System.out.println(output.toString());
+	}
+}
